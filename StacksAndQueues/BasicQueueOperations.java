c39feb95e062f49c2bf5c195c854a5f3c7c088bd
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-05-24 15:54:11 +0300

    Add files via upload

---
+++ StacksAndQueues/BasicQueueOperations.java
@@ -0,0 +1,40 @@
+package StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BasicQueueOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] input = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer :: parseInt).toArray();
+
+        int countToPush = input[0];
+        int countToPop = input[1];
+        int elementsToSearcg = input[2];
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer :: parseInt).toArray();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < countToPush; i++) {
+            stack.offer(numbers[i]);
+
+        }
+        for (int i = 0; i < countToPop; i++) {
+            stack.poll();
+
+        }
+        if (stack.contains(elementsToSearcg)){
+            System.out.println("true");
+        }else if (stack.isEmpty()){
+            System.out.println("0");
+        }else {
+            System.out.println(Collections.min(stack));
+        }
+
+    }
+}
+
+
