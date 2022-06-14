7bfb853c077a89356147db78ece75571d815dd99
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-06-14 17:40:26 +0300

    Add files via upload

---
+++ GenericsExercises/GenericBoxInteger/Box.java
@@ -0,0 +1,15 @@
+package GenericBoxInteger;
+
+public class Box<E> {
+    private E element;
+
+    public Box(E element) {
+        this.element = element;
+    }
+
+    @Override
+    public String toString() {
+        return element.getClass().getName() + ": " + element;
+    }
+
+}
