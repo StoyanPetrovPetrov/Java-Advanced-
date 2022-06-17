db2c879c2203eb08a611e956379b8aef75b56f8b
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-06-17 19:48:34 +0300

    Add files via upload

---
+++ IteratorsComparatorsExercises/ListyIterator/Collection/Main.java
@@ -0,0 +1,39 @@
+package Collection;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> inputList = Arrays.stream(scanner.nextLine().split("\\s+")).skip(1).collect(Collectors.toList());
+        ListyIterator listyIterator = new ListyIterator(inputList);
+
+        String secondInput = scanner.nextLine();
+
+        while (!secondInput.equals("END")) {
+
+            switch (secondInput) {
+                case "Move":
+                    System.out.println(listyIterator.move());
+                    break;
+
+                case "Print":
+                    listyIterator.print();
+                    break;
+
+                case "HasNext":
+                    System.out.println(listyIterator.hasNext());
+                    break;
+
+                case "PrintAll":
+                    listyIterator.pritAll();
+            }
+            secondInput = scanner.nextLine();
+
+        }
+    }
+}
