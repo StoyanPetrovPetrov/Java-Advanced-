18f3c3d7e11a62ee6767c5aab0eaf3c4ebcdc25a
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-06-17 20:10:48 +0300

    Add files via upload

---
+++ IteratorsComparatorsExercises/ListyIterator/StrategyPattern/AgeComparator.java
@@ -0,0 +1,10 @@
+package StrategyPattern;
+
+import java.util.Comparator;
+
+public class AgeComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person firstPerson, Person secondPerson) {
+        return Integer.compare(firstPerson.getAge(), secondPerson.getAge());
+    }
+}
